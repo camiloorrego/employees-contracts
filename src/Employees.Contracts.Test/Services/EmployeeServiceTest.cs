9806e4ebd2c3fe9f798d086b9ad3cc5077126f6e
Camilo_Orrego <Camilo_Orrego@PRICAMILOOM>
2019-10-17 14:27:30 -0500

    Se agrega pruebas a servicio

---
+++ src/Employees.Contracts.Test/Services/EmployeeServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Employees.Contracts.Domain.Entities;
+using Employees.Contracts.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Employees.Contracts.Test.Services
+{
+   public class EmployeeServiceTest
+    {
+        private readonly Mock<IEmployeeRepository> _employeeRepository;
+        private readonly EmployeeService _employeesService;
+        public EmployeeServiceTest()
+        {
+            _employeeRepository = new Mock<IEmployeeRepository>();
+            _employeesService = new EmployeeService(_employeeRepository.Object);
+        }
+
+        [Fact]
+        public async void GetAll()
+        {
+            var expected = new Employee[]
+                 {
+                    new HourlyEmployee { Id = 1, Name = "Employee 1" },
+                    new HourlyEmployee { Id = 2, Name = "Employee 2" },
+                    new HourlyEmployee { Id = 3, Name = "Employee 3" }
+                 };
+            _employeeRepository.Setup(x => x.GetEmployeesAsync()).ReturnsAsync(expected);
+
+            // Act
+            var result = await _employeesService.GetEmployees();
+
+            // Assert
+            var okResult = Assert.IsType<Employee[]>(result);
+            Assert.Equal(expected.Length, okResult.Length);
+
+        }
+    }
+}
